00e31321d2aee45f017af3a62e2e5f7c4fcc1855
tejaswininagtode <71727139+tejaswininagtode@users.noreply.github.com>
2021-08-17 23:37:16 +0530

    Update process.py

--- cgi-bin/process.py
+++ cgi-bin/process.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python3
+
+import xmltodict
+import json
+import parser
+import os
+import cgi
+import subprocess as sp
+import requests
+
+print("content-type: text/html")
+print()
+
+def vehicle_info(text):
+   r = requests.get(f'http://regcheck.org.uk/api/reg.asmx/CheckIndia?RegistrationNumber={str(text)}&username=Teju21@')
+   data= xmltodict.parse(r.content)
+   jdata=json.dumps(data)
+   df = json.loads(jdata)
+   df1 = json.loads(df['Vehicle']['vehicleJson'])
+   return df1
+
+form = cgi.FieldStorage()
+cmd = form.getvalue("x")
+print(cmd)
+
+x=vehicle_info(cmd)
+print(x)
 
